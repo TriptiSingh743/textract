07360dc1f79afd0bfdef409828388fce09de71e2
Tripti Singh <131510415+TriptiSingh743@users.noreply.github.com>
2024-07-27 21:22:09 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '1kcPoA8leT/XgwVD5hHN/fe+bOMExPKtfGS1/EMl'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',       # Localhost
